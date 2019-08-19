bc2babeb1486994bd706b659b3effbd50fc3a952
Mike Harley <mharley12345@gmail.com>
2019-08-19 12:34:24 -0700

    INIT

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
 
-const About = () => <h1>About</h1>;
+const About = () => <Router><h1>About</h1></Router>;
 
 export default About;
